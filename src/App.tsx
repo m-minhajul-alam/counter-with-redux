78d5cea0cab7d036313b2589402e90ababbb7071
Minhajul-alam <alam.minhajul@outlook.com>
2024-06-26 11:03:03 +0600

    Make counter functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,41 @@
+import { useDispatch, useSelector } from "react-redux";
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+} from "./redux/features/counterSlice";
+
 function App() {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+
   return (
     <div className="h-screen w-full flex justify-center items-center">
-      <div className="flex items-center border border-gray-400 rounded-md bg-slate-50 p-5">
-        <button className="px-3 py-2 bg-green-500 rounded-md text-xl text-white font-semibold">
-          Increment
-        </button>
-        <h1 className="text-3xl font-semibold mx-3">0</h1>
-        <button className="px-3 py-2 bg-red-500 rounded-md text-xl text-white font-semibold">
-          Decrement
-        </button>
+      <div className="flex flex-col items-center space-y-4 border border-gray-400 rounded-md bg-slate-50 p-5">
+        <h1 className="text-3xl text-center font-semibold mx-3 w-full border border-black rounded-md px-2">
+          {count}
+        </h1>
+        <div className="flex items-center gap-2">
+          <button
+            onClick={() => dispatch(incrementByAmount({ value: 5 }))}
+            className="px-3 py-2 bg-blue-500 rounded-md text-xl text-white font-semibold"
+          >
+            +5
+          </button>
+          <button
+            onClick={() => dispatch(increment())}
+            className="px-3 py-2 bg-green-500 rounded-md text-xl text-white font-semibold"
+          >
+            +1
+          </button>
+
+          <button
+            onClick={() => dispatch(decrement())}
+            className="px-3 py-2 bg-red-500 rounded-md text-xl text-white font-semibold"
+          >
+            -1
+          </button>
+        </div>
       </div>
     </div>
   );
---
+++ src/redux/features/counterSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = { count: 0 };
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.count++;
+    },
+    decrement: (state) => {
+      state.count--;
+    },
+    incrementByAmount: (state, action) => {
+      state.count = state.count + action.payload.value;
+    },
+  },
+});
+
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+
+export default counterSlice.reducer;
