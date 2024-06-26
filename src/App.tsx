4c956eb1c9a2291d4542c311e2c79f7ad1888bcd
Minhajul-alam <alam.minhajul@outlook.com>
2024-06-26 09:15:53 +0600

    make counter ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 function App() {
   return (
-    <div>
-      <h1>counter with redux</h1>
-      
+    <div className="h-screen w-full flex justify-center items-center">
+      <div className="flex items-center border border-gray-400 rounded-md bg-slate-50 p-5">
+        <button className="px-3 py-2 bg-green-500 rounded-md text-xl text-white font-semibold">
+          Increment
+        </button>
+        <h1 className="text-3xl font-semibold mx-3">0</h1>
+        <button className="px-3 py-2 bg-red-500 rounded-md text-xl text-white font-semibold">
+          Decrement
+        </button>
+      </div>
     </div>
   );
 }
